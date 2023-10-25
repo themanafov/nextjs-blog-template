3d86e78bf1cb81099df8adff4c22a155bf8eefcd
Mehman Manafov <mehmanmanafov@icloud.com>
2023-10-25 21:40:51 +0400

    deployment error fixed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,12 +5,12 @@ import { MetadataRoute } from "next";
 export default function sitemap() {
   const articleRoutes = allArticles.map((article) => ({
     lastModified: article.date,
-    url: `${siteConfig.url}${article.slug}`,
+    url: `${siteConfig.url}${article.href}`,
   }));
 
   const routes = allPages.map((page) => ({
     lastModified: new Date().toISOString().split("T")[0],
-    url: `${siteConfig.url}${page.href}`,
+    url: `${siteConfig.url}/${page.slug}`,
   }));
 
   return [...routes, ...articleRoutes] as MetadataRoute.Sitemap;
